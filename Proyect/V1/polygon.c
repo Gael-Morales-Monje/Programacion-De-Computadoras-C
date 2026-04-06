66489a4bcb31a36ceeb8659dfe4199179e3d1b86
Gael-Morales-Monje <gaelmoralesmonje@gmail.com>
2026-04-06 12:17:30 -0600

    avance

---
+++ Proyect/V1/polygon.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main() {
+    srand(time(NULL));
+
+
+    printf("<svg width='1000' height='1000' xmlns='http://www.w3.org/2000/svg'>\n");
+    int n;
+    for(int i = 0; i<56;i++) {
+        scanf("%d", &n);
+        int r,g,b;
+        r = rand() % 256;
+        g = rand() % 256;
+        b = rand() % 256;
+        printf("<polygon stroke='rgb(%d,%d,%d)' stroke-width='3' fill='rgb(%d,%d,%d)' points='", r, g, b, b,g,r);
+        float x, y;
+        for (int i = 0; i < n; i++) {
+            scanf("%f %f", &x, &y);
+            printf("%f,%f ", x*35.8, y*35.8);
+        }
+        printf("'/>\n");
+        printf("<text x='%f' y='%f' font-size='20' fill='black'>%d</text>\n", x*35.8, y*35.8, i+1);
+    }
+    printf("</svg>\n");
+    return 0;
+} 
---
+++ Proyect/V1/polygon2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main() {
+    // srand(time(NULL));
+
+
+    printf("<svg width='1000' height='1000' xmlns='http://www.w3.org/2000/svg'>\n");
+    int n;
+    for(int i = 0; i<56;i++) {
+        scanf("%d", &n);
+        
+        printf("<polygon stroke='black' stroke-width='3' fill='red' points='");
+        float x, y;
+        for (int i = 0; i < n; i++) {
+            scanf("%f %f", &x, &y);
+            printf("%f,%f ", x*35.8, y*35.8);
+        }
+        printf("'/>\n");
+        // printf("<text x='%f' y='%f' font-size='20' fill='black'>%d</text>\n", x*35.8, y*35.8, i+1);
+    }
+    printf("</svg>\n");
+    return 0;
+} 
